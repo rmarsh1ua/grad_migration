f5c8a7dce3a3195b8834ad529a7047d47c8cfcc8
Lee Marsh <rmarsh1@arizona.edu>
2022-04-06 08:52:57 -0700

    Adding a utility source plugin that allows to filter nodes by a cutoff date, the intention being to easily exclude all old content that hasn't been modified in X number of years.

---
+++ src/Plugin/migrate/source/FilterOldContent.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\gc_migration\Plugin\migrate\source;
+
+use Drupal\node\Plugin\migrate\source\d7\Node as D7Node;
+
+/**
+ * Drupal 7 file source (optionally filtered by type) from database.
+ *
+ * @MigrateSource(
+ *   id = "filter_old_content"
+ * )
+ */
+class FilterOldContent extends D7Node
+{
+
+  /**
+   * {@inheritdoc}
+   */
+    public function query()
+    {
+        /**
+         * Filter date from migration script can be a
+         * string date or a UNIX timestamp so get
+         * it into correct format
+         */
+        if (is_int($this->configuration['filter_date'])) {
+            $date_cutoff = $this->configuration['filter_date'];
+        } else {
+            $date_cutoff = (new \DateTime($this->configuration['filter_date']))->getTimestamp();
+        }
+
+        $query = parent::query();
+        $query->condition('n.changed', $date_cutoff, '>');
+
+        return $query;
+    }
+}
